8fb9c37586a73cc609c75b0518c9f380a5a1d377
Kmontano06 <keniamontano06@gmail.com>
2015-11-06 14:23:19 -0600

    Cambios en nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 $('#menu').click(function(){
-	$('.list-nav').toggle();
+	$('.container-nav').toggle();
 	console.log('blah');
+});
+$(window).scroll(function() {
+	//condition for scroll position
+  if ($(this).scrollTop() > 87){  
+    //make CSS changes here
+    $('.container-nav').addClass("sticky");
+  }else{
+    //back to default styles
+    $('.container-nav').removeClass("sticky");
+  }
 });
